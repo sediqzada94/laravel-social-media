89ba8aebda5aa830316d10b7c9befbd0e93e35f1
Ghulam Muhy Din Nazamey <35891049+nazamey@users.noreply.github.com>
2025-12-20 07:09:23 +0430

    Update users link to match route pattern

--- resources/views/components/app-sidebar.blade.php
+++ resources/views/components/app-sidebar.blade.php
@@ -8,8 +8,8 @@
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('users.index') }}" class="flex items-center px-2 py-1.5 text-body rounded-base  hover:bg-blue-500 group {{ request()->routeIs('users.index') ? 'bg-green-600 text-white' : 'text-black bg-white border-red-500 border-2' }}"">
-                    <svg class="shrink-0 w-5 h-5 transition duration-75 {{ request()->routeIs('users.index') ? 'text-white' : 'group-hover:text-fg-brand' }}" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v14M9 5v14M4 5h16a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z"/></svg>
+                    <a href="{{ route('users.index') }}" class="flex items-center px-2 py-1.5 text-body rounded-base  hover:bg-blue-500 group {{ request()->routeIs('users.*') ? 'bg-green-600 text-white' : 'text-black bg-white border-black-500 border-2' }}"">
+                    <svg class="shrink-0 w-5 h-5 transition duration-75 {{ request()->routeIs('users.*') ? 'text-white' : 'group-hover:text-fg-brand' }}" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v14M9 5v14M4 5h16a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z"/></svg>
                     <span class="flex-1 ms-3 whitespace-nowrap">Users</span>
                     </a>
                 </li>
